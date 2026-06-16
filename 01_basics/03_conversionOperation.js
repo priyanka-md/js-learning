80fe2a90a0607508d34df282d90310595d26fe9e
priyanka-md <146166215+priyanka-md@users.noreply.github.com>
2026-06-16 13:00:50 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,14 @@
 // let score = '33abc'
 let score = null
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -17,7 +17,7 @@ console.log( valueInNumber);
 let isLoggedIn = 0
 
 let booleanIsLoogedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoogedIn);
+// console.log(booleanIsLoogedIn);
 
 
 // 1=> true; 0 => false
@@ -28,5 +28,44 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ******************** Operations ************************//
+
+let value = 3
+let negValue = -value
+
+console.log(value);
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) //power
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2 
+console.log(str3); // hello world
+
+console.log(+true);
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++; // Prefix and Postfix operator
+console.log(gameCounter);
+
+
+
+
+
