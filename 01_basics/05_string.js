622c68081f52c7c45d3f065d5c3017cf38c2febf
priyanka-md <146166215+priyanka-md@users.noreply.github.com>
2026-06-17 11:37:03 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "Test"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+const gameName = new String("TestGame")
+console.log(gameName);
+console.log(gameName.length);
